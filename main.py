1195b92acda3026aab6f563250fd4571f3e43445
Roy <royroy.euph@gmail.com>
2019-12-17 20:38:54 +0900

    DiscordのチャンネルIDをファイル内で指定して、FFLogs WebhookのURLを取得する機能を追加

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import sys, time, os, json, array, requests, tqdm
-from selenium import webdriver
+import sys
+import time
+import requests
+import tqdm
+
 from statistics import mean
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 import chromedriver_binary
+from bs4 import BeautifulSoup
+
+import asyncio
+import discord
+import traceback
+
+# FFLogs APIキー
+FFLOGS_API_KEY = ''
+
+# Discord Bot アクセストークン
+DISCORD_TOKEN = ''
+
+# Discord テキストチャンネルID
+DISCORD_CHANNEL_ID = 000000000000000000
 
 # ソート用ジョブ名配列
 JOB_SORT_RANK = ['DarkKnight', 'Warrior', 'Gunbreaker', 'Paladin', 'WhiteMage', 'Astrologian', 'Scholar', 'Samurai', 'Monk', 'Dragoon', 'Ninja', 'Bard', 'Machinist', 'Dancer', 'BlackMage', 'Summoner', 'RedMage', 'Total']
@@ -14,26 +30,69 @@ FFLOGS_API_FIGHT_URL = 'https://www.fflogs.com/v1/report/fights/{report_id}?api_
 FFLOGS_DPS_URL = 'https://www.fflogs.com/reports/{report_id}#boss={boss_id}&difficulty=100&type=damage-done&phase={phase_num}'
 FFLOGS_URL_FIGHT_QUERY = '&fight={fight_id}'
 
-FFLOGS_API_KEY = ''
-# FFLogs APIキー
+# 引数処理
+if len(sys.argv) <= 1:
+    class DiscordClient(discord.Client):
+        '''
+        コールバック関数を設定できる、discord.Clientを継承したクラス
+        '''
+        def __init__(self, callback=None, loop=None, **options):
+            super(DiscordClient, self).__init__(loop=loop, **options)
+            self.callback = callback
+
+        # 接続完了時の処理
+        async def on_ready(self):
+            channel = self.get_channel(DISCORD_CHANNEL_ID)
+            if channel is None:
+                await self.close()
+                raise RuntimeError('Channel ID "{}" is not found.'.format(DISCORD_CHANNEL_ID))
+            
+            for message in await channel.history().flatten():
+                if len(message.embeds) <= 0:
+                    continue
+
+                if message.embeds[0].url.startswith('https://') and 'fflogs.com/reports/' in message.embeds[0].url:
+                    if self.callback is not None:
+                        self.callback(message.embeds[0].url.split('/')[4])
+                    break
+            await self.close()
+
+    # コールバック用データとコールバック関数
+    callback_data = []
+    def callback(data):
+        callback_data.append(data)
+
+    # Discordに接続
+    client = DiscordClient(callback)
+    client.run(DISCORD_TOKEN)
+
+    # コールバックが呼び出されていない場合はエラー
+    if len(callback_data) == 0:
+        raise RuntimeError('Failed to get report ID from Discord.')
+
+    # コールバック用リストからレポートIDを設定
+    report_id = callback_data[0]
+
+elif sys.argv[1].startswith('https://') and 'fflogs.com/reports/' in sys.argv[1]:
+    report_id = sys.argv[1].split('/')[4]
+else:
+    report_id = sys.argv[1]
 
 class Actor:
-	def __init__(self, name, job, phase_count):
-		self.name = name
-		self.job = job
-		self.dps = [0] * phase_count
+    '''
+    Actor: プレイヤーなどのキャラクターを表すクラス
+    '''
+    def __init__(self, name, job, phase_count):
+        self.name = name
+        self.job = job
+        self.dps = [0] * phase_count
 
-	def __lt__(self, other):
-		# self < other
-		return JOB_SORT_RANK.index(self.job) < JOB_SORT_RANK.index(other.job)
+    def __lt__(self, other):
+        # self < other
+        return JOB_SORT_RANK.index(self.job) < JOB_SORT_RANK.index(other.job)
 
-	def __repr__(self):
-		return self.name + ': ' + str(self.dps)
-
-if sys.argv[1].startswith('https://') and 'fflogs.com/reports/' in sys.argv[1]:
-	report_id = sys.argv[1].split('/')[4]
-else:
-	report_id = sys.argv[1]
+    def __repr__(self):
+        return self.name + ': ' + str(self.dps)
 
 # FFLogs APIから戦闘情報を取得
 res = requests.get(FFLOGS_API_FIGHT_URL.format(report_id=report_id, api_key=FFLOGS_API_KEY))
@@ -42,87 +101,88 @@ res.raise_for_status()
 # フェーズ/インターミッション情報を戦闘情報から取得
 fights_data = res.json()
 for phase in fights_data['phases']:
-	if phase['boss'] == FFLOGS_TARGET_BOSS_ID:
-		phases = phase['phases']
-		intermissions = phase['intermissions']
-		break
+    if phase['boss'] == FFLOGS_TARGET_BOSS_ID:
+        phases = phase['phases']
+        intermissions = phase['intermissions']
+        break
 
-# プログレスバー初期化
-pbar = tqdm.tqdm(total=len(phases) * len(fights_data['fights']))
+fight_times = [[] for i in range(len(phases))]
 
 # プレイヤー名を戦闘情報から取得
 dps_table = { 'Total': Actor('Total', 'Total', len(phases)) }
 for friendly in fights_data['friendlies']:
-	if friendly['type'] in JOB_SORT_RANK:
-		dps_table[friendly['name']] = Actor(friendly['name'], friendly['type'], len(phases))
+    if friendly['type'] in JOB_SORT_RANK:
+        dps_table[friendly['name']] = Actor(friendly['name'], friendly['type'], len(phases))
 
-# Selenium Google Chrome Driver
-options = Options()
-options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
+# プログレスバー初期化
+with tqdm.tqdm(total=len(phases) * len(fights_data['fights'])) as pbar:
 
-fight_times = []
-for i in range(1, len(phases) + 1):
-	if i not in intermissions:
-		driver.get(FFLOGS_DPS_URL.format(report_id=report_id, boss_id=FFLOGS_TARGET_BOSS_ID, phase_num=i))
-		time.sleep(1.0)
-		html_table = BeautifulSoup(driver.page_source.encode('utf-8'), 'html.parser').find('table', id='main-table-0')
+    # Google Chrome Driver Optionsの設定 for Headless mode
+    options = webdriver.chrome.options.Options()
+    options.add_argument('--headless')
 
-		if html_table is None:
-			continue
+    # Selenium Google Chrome Driver
+    with webdriver.Chrome(options=options) as driver:
+        for i in range(1, len(phases) + 1):
+            if i not in intermissions:
+                driver.get(FFLOGS_DPS_URL.format(report_id=report_id, boss_id=FFLOGS_TARGET_BOSS_ID, phase_num=i))
+                time.sleep(1.0)
+                html_table = BeautifulSoup(driver.page_source.encode('utf-8'), 'html.parser').find('table', id='main-table-0')
 
-		for row in html_table.find('tbody').find_all('tr'):
-			name_cell = row.find('td', {'class': 'report-table-name'})
-			dps_cell = row.find('td', {'class': 'primary', 'class': 'main-per-second-amount'})
+                if html_table is None:
+                    continue
 
-			if name_cell is None or dps_cell is None:
-				continue
-			
-			name_text = name_cell.get_text().replace('\n', '')
-			if name_text in dps_table:
-				dps_text = dps_cell.get_text().replace('\n', '').replace('\t', '').replace(',', '')
-				dps_table[name_text].dps[i - 1] = float(dps_text)
-			
-			total_dps_text = html_table.find('tfoot').find('tr').find_all('td')[3].get_text()
-			dps_table['Total'].dps[i - 1] = float(total_dps_text.replace('\n', '').replace('\t', '').replace(',', ''))
+                for row in html_table.find('tbody').find_all('tr'):
+                    name_cell = row.find('td', {'class': 'report-table-name'})
+                    dps_cell = row.find('td', {'class': 'primary', 'class': 'main-per-second-amount'})
 
-	fight_times.append([])
-	for fight in fights_data['fights']:
-		pbar.update()
-		if fight['zoneID'] == FFLOGS_TARGET_ZONE_ID:
-			driver.get(FFLOGS_DPS_URL.format(report_id=report_id, boss_id=FFLOGS_TARGET_BOSS_ID, phase_num=i) + FFLOGS_URL_FIGHT_QUERY.format(fight_id=fight['id']))
-			time.sleep(0.4)
-			html_table = BeautifulSoup(driver.page_source.encode('utf-8'), 'html.parser').find('table', id='main-table-0')
+                    if name_cell is None or dps_cell is None:
+                        continue
+                    
+                    name_text = name_cell.get_text().replace('\n', '')
+                    if name_text in dps_table:
+                        dps_text = dps_cell.get_text().replace('\n', '').replace('\t', '').replace(',', '')
+                        dps_table[name_text].dps[i - 1] = float(dps_text)
+                    
+                    total_dps_text = html_table.find('tfoot').find('tr').find_all('td')[3].get_text()
+                    dps_table['Total'].dps[i - 1] = float(total_dps_text.replace('\n', '').replace('\t', '').replace(',', ''))
 
-			if html_table is None:
-				continue
+            for fight in fights_data['fights']:
+                pbar.update()
+                if fight['zoneID'] == FFLOGS_TARGET_ZONE_ID:
+                    driver.get(FFLOGS_DPS_URL.format(report_id=report_id, boss_id=FFLOGS_TARGET_BOSS_ID, phase_num=i) + FFLOGS_URL_FIGHT_QUERY.format(fight_id=fight['id']))
+                    time.sleep(0.4)
+                    html_table = BeautifulSoup(driver.page_source.encode('utf-8'), 'html.parser').find('table', id='main-table-0')
 
-			active_time_text = html_table.find('tfoot').find('tr').find_all('td')[2].get_text()
-			active_time = float(active_time_text.replace('s', '').replace('\n', '').replace(',', ''))
-			if active_time > 0.0:
-				fight_times[i - 1].append(active_time)
+                    if html_table is None:
+                        continue
 
-pbar.close()
-driver.close()
+                    active_time_text = html_table.find('tfoot').find('tr').find_all('td')[2].get_text()
+                    active_time = float(active_time_text.replace('s', '').replace('\n', '').replace(',', ''))
+                    try:
+                        if active_time > 0.0:
+                            fight_times[i - 1].append(active_time)
+                    except:
+                        print('i={} : {}'.format(i, traceback.format_exc()))
+                        raise
 
 # DPSテーブルが空のプレイヤーを除外
 for name in list(dps_table.keys()):
-	if sum(dps_table[name].dps) == 0:
-		del dps_table[name]
+    if sum(dps_table[name].dps) == 0:
+        del dps_table[name]
 
-print('###################################')
-print(' Results')
-print('###################################')
 actor_list = list(dps_table.values())
 actor_list.sort()
 
-result_text = ''
+result_text =  '###################################\n' \
+               ' Results\n' \
+               '###################################\n'
 for i in range(1, len(phases) + 1):
-	for actor in actor_list:
-		result_text += str(actor.dps[i - 1]) + '\t'
-	if len(fight_times[i - 1]) > 0:
-		result_text += str(max(fight_times[i - 1]) ) + '\t' + str(mean(fight_times[i - 1])) + '\n'
-	else:
-		result_text += '\t\n'
+    for actor in actor_list:
+        result_text += str(actor.dps[i - 1]) + '\t'
+    if len(fight_times[i - 1]) > 0:
+        result_text += str(max(fight_times[i - 1]) ) + '\t' + str(mean(fight_times[i - 1])) + '\n'
+    else:
+        result_text += '\t\n'
 
 print(result_text)
